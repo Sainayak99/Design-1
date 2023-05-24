02d936d7d17816310cf90a2910a9d6772f6f1b95
sainath k <sainathkodiripaka99@gmail.com>
2023-05-23 19:42:24 -0500

    Done

--- Sample.java
+++ Sample.java
@@ -5,3 +5,24 @@
 
 
 // Your code here along with comments explaining your approach
+
+import java.util.HashSet;
+
+class MyHashSet {
+    HashSet<Integer> set;
+    public MyHashSet() {
+        set = new HashSet<Integer>();
+    }
+    
+    public void add(int key) {
+        set.add(key);
+    }
+    
+    public void remove(int key) {
+        set.remove(key);
+    }
+    
+    public boolean contains(int key) {
+        return set.contains(key);
+    }
+}
